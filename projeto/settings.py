b62673a18b489e03f7d208b9bfbd2f83465d2aee
victorAMSP <victor.a.p@hotmail.com>
2025-01-20 10:33:06 -0300

    criacao do app authors

--- projeto/settings.py
+++ projeto/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
+    'authors',
     
 ]
 
